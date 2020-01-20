fb5866d4e94d412d8bd18ad9117797bca7124447
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-20 14:56:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function nowServing (katzDeliLine){
  return "There is nobody waiting to be served!";
  } 
  else 
- return ("Currently serving [katzDeliLine.shift()].")
+ return ("Currently serving " + katzDeliLine.shift() + ".")
 }

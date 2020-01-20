57a54d67f4e8735df1b66fe413ae160a0fe088cd
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-20 15:04:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nowServing (katzDeliLine){
 var line = []
 function currentLine(katzDeliLine){
 for ( let i = 0; i < katzDeliLine.length; i++){
-  
+  line.push()
 }
     if (katzDeliLine.length === 0){
       return ("The line is currently empty.")
